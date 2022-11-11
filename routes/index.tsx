f20a18af4c24b80bf23f4f163dd57d0e7f3a9497
MrAmericanMike <mramericanmike@gmail.com>
2022-11-11 19:35:26 -0300

    Lemons

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh Lemons</title>
+		<link rel="stylesheet" href="styles.css"></link>
+      </Head>
+      <div id="lemon">
+        <img
+          src="/logo.svg"
+          alt="Fresh Lemons"
+        />
+		FRESH LEMONS
+      </div>
+    </>
+  );
+}
