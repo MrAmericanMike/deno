eb8acc96d6a46d9cc8124d3c30bf8e9fec87c9ed
MrAmericanMike <mramericanmike@gmail.com>
2022-11-12 11:27:49 -0300

    Lemons

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,13 +3,13 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/joke.ts";
+import * as $0 from "./routes/api/lemon.ts";
 import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/joke.ts": $0,
+    "./routes/api/lemon.ts": $0,
     "./routes/index.tsx": $1,
   },
   islands: {
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,19 +1,17 @@
 import { Head } from "$fresh/runtime.ts";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Fresh Lemons</title>
-		<link rel="stylesheet" href="styles.css"></link>
-      </Head>
-      <div id="lemon">
-        <img
-          src="/logo.svg"
-          alt="Fresh Lemons"
-        />
-		FRESH LEMONS
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Fresh Lemons</title>
+				<link rel="stylesheet" href="styles.css"></link>
+			</Head>
+			<div id="lemon">
+				<img src="/logo.svg" alt="Fresh Lemons" />
+				FRESH LEMONS
+			</div>
+		</>
+	);
 }
+
